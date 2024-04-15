11b72b5ecea7000250ae43698d3cab194979f149
Kevin Ayerdi <kevin.ayerdi@hotmail.com>
2024-04-15 15:24:56 -0300

    Agregando boton de eliminar actividad

--- src/reducers/activity-reducer.ts
+++ src/reducers/activity-reducer.ts
@@ -2,7 +2,9 @@ import { Activity } from "../types"
 
 export type ActivityActions =
 { type: 'save-activity', payload: {newActivity : Activity}} |
-{ type: 'set-activeId', payload: {id : Activity['id']}}
+{ type: 'set-activeId', payload: {id : Activity['id']}} |
+{ type: 'delete-activity', payload: {id : Activity['id']}}
+
 
 
 export type ActivityState = {
@@ -41,6 +43,13 @@ export const activityReducer = (
     return {
       ...state,
       activeId: action.payload.id
+    }
+  }
+
+  if(action.type === 'delete-activity'){
+    return {
+      ...state,
+      activities: state.activities.filter( activity => activity.id !== action.payload.id)
     }
   }
   return state
