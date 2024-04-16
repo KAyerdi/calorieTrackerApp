d7f1f3982887f306f4e274ac84c119f1d1d9f1e0
Kevin Ayerdi <kevin.ayerdi@hotmail.com>
2024-04-16 01:07:59 -0300

    Actualizacion

--- src/components/CaloriesTracker.tsx
+++ src/components/CaloriesTracker.tsx
@@ -6,16 +6,20 @@ type CalorieTrackerProps = {
 }
 
 export default function CaloriesTracker({activities} : CalorieTrackerProps) {
-  //Contadores
+
+  //Contador consumidas
 
   const caloriesConsumed = useMemo(() => activities.reduce((total, activity) => activity.category === 1 ? total + activity.calories : total, 0) ,[activities])
 
+    //Contador ejercicio
   const caloriesBurned = useMemo(() => activities.reduce((total, activity) => activity.category === 2 ? total + activity.calories : total, 0) ,[activities])
+
   return (
     <>
       <h2 className='text-4xl font-black text-white text-center'>
         Resumen de Calorias
       </h2>
+      
       <div className='flex flex-col items-center md:flex-row md:justify-between gap-5 mt-10'>
         <p className='text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center'>
           <span className='font-black text-6xl text-orange'>
@@ -29,7 +33,6 @@ export default function CaloriesTracker({activities} : CalorieTrackerProps) {
           </span>
           Ejercicio
         </p>
-        //POR ALGUN MOTIVO NO SE MEUSTRAN LAS CALORIAS QUEMADAS X EJERCICIO. CHEQUEAR.
       </div>
     </>
   );
